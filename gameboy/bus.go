e904b41fd28924e0a8b5884c81ba250cdc9538ab
codeFrite <your_email@example.com>
2024-06-10 11:42:19 +0200

    adding write func to the bus. Writes a byte to the cartridge, VRAM or WRAM depending on the address: func (b *Bus) Write(addr [2]byte, value byte)

--- gameboy/bus.go
+++ gameboy/bus.go
@@ -25,3 +25,14 @@ func (b *Bus) Read(addr [2]byte) byte {
 	}
 	return 0
 }
+
+func (b *Bus) Write(addr [2]byte, value byte) {
+	addr16 := bytesToUint16(addr)
+	if addr16 <= 0x7FFF {
+		b.cartridge.Write(addr, value)
+	} else if addr16 >= 0x8000 && addr16 <= 0x9FFF {
+		b.vram.Write(addr16 - 0x8000, value)
+	} else if addr16 >= 0xC000 && addr16 <= 0xDFFF {
+		b.wram.Write(addr16 - 0xC000, value)
+	}
+}
