86ec30dfd6fc2cb3bd0bdc634df4b8e9dbe25245
codeFrite <your_email@example.com>
2024-06-17 14:13:18 +0200

    test.sh: adding yellow formatting for skipped TCs

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
 go test -v ./gameboy | while read line; do
@@ -9,6 +10,8 @@ go test -v ./gameboy | while read line; do
     echo -e "${RED}${line}${NC}"
   elif echo "$line" | grep -q "PASS"; then
     echo -e "${GREEN}${line}${NC}"
+  elif echo "$line" | grep -q "SKIP"; then
+    echo -e "${YELLOW}${line}${NC}"
   else
     echo "$line"
   fi
