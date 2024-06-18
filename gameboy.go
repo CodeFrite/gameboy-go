bac74cc5596f78b3963be45b4c674909fc846335
codeFrite <codefrite@gmail.com>
2024-06-19 00:45:49 +0200

    gameboy: add initCPU func to simulate the effect of the boot room which sets some registers and memory location to particular values (init not yet completed)

--- gameboy.go
+++ gameboy.go
@@ -7,6 +7,15 @@ import (
 	"codefrite.dev/emulators/gameboy"
 )
 
+// TODO> Since the boot rom is not run on init, I should make sure to init the gameboy (CPU, BUS, WRAM, VRAM, etc) properly.
+// TODO> There is more to it than setting the PC to 0x0100 and SP to 0xFFFE.
+func initCPU(cpu *gameboy.CPU) {
+	// set CPU PC to 0x100 to skip the boot rom and start executing the game
+	cpu.PC = 0x0100
+
+	// ! the SP is normally set to 0xFFFE by the boot rom. We will set it manually for the moment
+	cpu.SP = 0xFFFE
+}
 func initGameboy() *gameboy.CPU {
 	// 1. Init VRAM
 	vram := gameboy.NewMemory(0x2000)
@@ -38,6 +47,9 @@ func initGameboy() *gameboy.CPU {
 func main() {
 	// init gameboy
 	cpu := initGameboy()
+
+	// init CPU since the boot rom is not run
+	initCPU(cpu)
 
 	// print cpu info
 	//cpu.PrintRegisters()
