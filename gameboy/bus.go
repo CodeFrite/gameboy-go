b038178783b211c175abf4af01922cb0aa04b37e
codeFrite <codefrite@gmail.com>
2024-06-18 22:29:32 +0200

    bus: adding the func (b *Bus) Read16(addr uint16) uint16to read 2 consecutives bytes from the bus and return it as little-endian

--- gameboy/bus.go
+++ gameboy/bus.go
@@ -10,7 +10,7 @@ type Accessible interface {
 
 type MemoryMap struct {
 	Address uint16
-	Memory Accessible
+	Memory  Accessible
 }
 
 type Bus struct {
@@ -26,16 +26,16 @@ func NewBus() *Bus {
 func (b *Bus) AttachMemory(memory Accessible, address uint16) {
 	b.router = append(b.router, MemoryMap{
 		Address: address,
-		Memory: memory,
+		Memory:  memory,
 	})
 }
 
 /*
  * Return the memory map that contains the address or return nil
  */
-func (b *Bus) findMemory(address uint16) (*MemoryMap,error) {
+func (b *Bus) findMemory(address uint16) (*MemoryMap, error) {
 	for _, memoryMap := range b.router {
-		if address >= memoryMap.Address && address < memoryMap.Address + memoryMap.Memory.Size(){
+		if address >= memoryMap.Address && address < memoryMap.Address+memoryMap.Memory.Size() {
 			return &memoryMap, nil
 		}
 	}
@@ -51,10 +51,14 @@ func (b *Bus) Read(addr uint16) uint8 {
 	}
 }
 
+func (b *Bus) Read16(addr uint16) uint16 {
+	return uint16(b.Read(addr+1))<<8 | uint16(b.Read(addr))
+}
+
 func (b *Bus) Write(addr uint16, value uint8) {
 	memoryMap, err := b.findMemory(addr)
 	if err == nil {
-		memoryMap.Memory.Write(addr - memoryMap.Address, value)
+		memoryMap.Memory.Write(addr-memoryMap.Address, value)
 	} else {
 		panic(err)
 	}
