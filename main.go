3d1c23b6b74ce5402a3238891c23050bc7ef6378
codeFrite <codefrite@gmail.com>
2024-06-22 07:48:04 +0200

    rename gameboy.go -> main.go

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	"codefrite.dev/emulators/gameboy"
+)
+
+func main() {
+	// Create a new Gameboy
+	gb := gameboy.NewGameboy()
+	gb.Run("./roms/", "tetris.gb")
+
+	// Exit program properly
+	os.Exit(0)
+}
