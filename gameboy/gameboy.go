104c5d844e9ea4b9beb661d1292ea08f17021853
codeFrite <codefrite@gmail.com>
2024-06-22 07:49:33 +0200

    adding file gameboy/gameboy.go to initialize a gameboy CPU/BUS and its memory along with a memory to hold the boot rom

---
+++ gameboy/gameboy.go
@@ -0,0 +1,82 @@
+package gameboy
+
+import (
+	"log"
+	"os"
+)
+
+type Gameboy struct {
+	cpu          *CPU
+	bootrom      *Memory
+	cartridge    *Cartridge
+	vram         *Memory
+	wram         *Memory
+	io_registers *Memory
+	bus          *Bus
+}
+
+func NewGameboy() *Gameboy {
+	g := &Gameboy{}
+	g.initMemory()
+	g.initBus()
+	g.initCPU()
+	return g
+}
+
+// Initializers
+
+func (g *Gameboy) initMemory() {
+	// Bootrom
+	g.bootrom = NewMemory(0x0100)
+	// VRAM
+	g.vram = NewMemory(0x2000)
+	// WRAM
+	g.wram = NewMemory(0x2000)
+	// I/O Registers
+	g.io_registers = NewMemory(0x007F)
+}
+
+func (g *Gameboy) initBus() {
+	g.bus = NewBus()
+	g.bus.AttachMemory(g.vram, 0x8000)
+	g.bus.AttachMemory(g.wram, 0xC000)
+	g.bus.AttachMemory(g.io_registers, 0xFF00)
+}
+
+func (g *Gameboy) initCPU() {
+	g.cpu = NewCPU(g.bus)
+}
+
+// Utility functions
+func getBootRomData() []uint8 {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rom, err := LoadRom(currentDir + "/roms/dmg_boot.bin")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return rom
+}
+
+func (g *Gameboy) loadCartridge(uri string, name string) {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	g.cartridge = NewCartridge(currentDir+uri, name)
+	g.bus.AttachMemory(g.cartridge, 0x0000)
+}
+
+/*
+ * Run the bootrom and then the game
+ */
+func (g *Gameboy) Run(uri string, name string) {
+	bootRomData := getBootRomData()
+	g.bus.AttachMemory(g.bootrom, 0x0000)
+	g.bus.WriteBlob(0x0000, bootRomData)
+	g.cpu.Boot()
+	g.loadCartridge(uri, name)
+	g.cpu.Run()
+}
