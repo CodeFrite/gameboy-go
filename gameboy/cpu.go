be4d98ac13b34298fe8de5a414e81723cfdbb33e
codeFrite <codefrite@gmail.com>
2024-07-05 18:11:57 +0200

    cpu: all members of CPU struct are now public to be accessible by JSON functions // fetchOperandValue and fetchOpCode do not update the cpu state anymore but rather return the value to the caller // minor cosmetic changes (comments and variables order)

--- gameboy/cpu.go
+++ gameboy/cpu.go
@@ -9,21 +9,25 @@ import (
  * CPU: executes instructions fetched from memory, reads and writes to memory (internal registers, flags & bus)
  */
 type CPU struct {
+	// Registers
 	PC               uint16 // Program Counter
 	SP               uint16 // Stack Pointer
 	A                uint8  // Accumulator
 	F                uint8  // Flags: Zero (position 7), Subtraction (position 6), Half Carry (position 5), Carry (position 4)
 	B, C, D, E, H, L uint8  // 16-bit general purpose registers
-	IE               uint8  // Interrupt Enable
 
+	// Instruction
 	IR       uint8  // Instruction Register
-	prefixed bool   // Is the current instruction prefixed with 0xCB
+	Prefixed bool   // Is the current instruction prefixed with 0xCB
 	Operand  uint16 // Current operand fetched from memory (this register doesn't physically exist in the CPU)
 
+	// Memory
 	HRAM [127]byte // 127bytes of High-Speed RAM
 	bus  *Bus      // reference to the bus
 
-	IME    bool // interrupt master enable
+	// Interrupts
+	IE     uint8 // Interrupt Enable
+	IME    bool  // interrupt master enable
 	halted bool
 }
 
@@ -32,6 +36,11 @@ func NewCPU(bus *Bus) *CPU {
 	return &CPU{
 		bus: bus,
 	}
+}
+
+// Increment the Program Counter by the given offset
+func (c *CPU) incrementPC(offset uint16) {
+	c.PC += uint16(offset)
 }
 
 // Stack operations
@@ -62,26 +71,24 @@ func (c *CPU) pop() uint16 {
 }
 
 // Fetch the opcode from bus at address PC and store it in the instruction register
-func (c *CPU) fetchOpcode() {
+func (c *CPU) fetchOpcode() (opcode uint8, prefixed bool) {
 	// Fetch the opcode from memory at the address in the program counter
-	opcode := c.bus.Read(c.PC)
+	opcode = c.bus.Read(c.PC)
 
 	// is it a prefixed instruction?
 	if opcode == 0xCB {
-		c.prefixed = true
+		prefixed = true
 		// fetch the next opcode
 		opcode = c.bus.Read(c.PC + 1)
 	}
-
-	// Store the opcode in the instruction register
-	c.IR = opcode
+	return opcode, prefixed
 }
 
 /*
  * Fetch the value of an operand
  * Save the result in cpu.Operand as an uint16 (must be casted to the correct type inside the different instruction handlers)
  */
-func (c *CPU) fetchOperandValue(operand Operand) {
+func (c *CPU) fetchOperandValue(operand Operand) uint16 {
 	var value, addr uint16
 	switch operand.Name {
 	case "n8": // always immediate
@@ -193,38 +200,35 @@ func (c *CPU) fetchOperandValue(operand Operand) {
 		err := fmt.Sprintf("Unknown operand name: %s (0x%02X)", operand.Name, c.IR)
 		panic(err)
 	}
-	// save the current operand value into the cpu context
-	c.Operand = value
-}
-
-func (c *CPU) incrementPC(offset uint16) {
-	c.PC += uint16(offset)
+	return value
 }
 
 // Execute one cycle of the CPU: fetch, decode and execute the next instruction
 // TODO: i am supposed to return an error but i am always returning nil. Chose an error handling strategy and implement it
 func (c *CPU) step() error {
 	// 0. reset the prefixed flag
-	c.prefixed = false
+	c.Prefixed = false
 
-	// 1. Fetch the opcode from memory and save it to the instruction register IR
-	c.fetchOpcode()
+	// 1. Store the opcode in the instruction register & prefix flag
+	opCode, prefixed := c.fetchOpcode()
+	c.Prefixed = prefixed
+	c.IR = opCode
 
 	// 2. Decode the instruction
 	// get instruction from opcodes.json file with IR used as key
-	instruction := GetInstruction(Opcode(fmt.Sprintf("0x%02X", c.IR)), c.prefixed)
+	instruction := GetInstruction(Opcode(fmt.Sprintf("0x%02X", c.IR)), c.Prefixed)
 	// get the operands of the instruction
 	operands := instruction.Operands
 	// fetch the operand value
 	if len(operands) == 1 {
-		c.fetchOperandValue(operands[0])
+		c.Operand = c.fetchOperandValue(operands[0])
 	} else if len(operands) == 2 {
 		// decode operand 2
-		c.fetchOperandValue(operands[1])
+		c.Operand = c.fetchOperandValue(operands[1])
 	}
 
 	// 3. Execute the instruction
-	if !c.prefixed {
+	if !c.Prefixed {
 		c.executeInstruction(instruction)
 	} else {
 		c.executeCBInstruction(instruction)
