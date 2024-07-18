ede3a092e52c77b3f5a860e6269cbdd975618d45
codeFrite <codefrite@gmail.com>
2024-07-18 20:31:18 +0200

    memory refactoring: removing setData func and replacing it with NewMemoryWithData to avoid memory changing its size // adding NewMemoryWithRandomData func to simulate the initial state of the VRAM on gameboy startup // adding tests to make the memory panic if we attempt to read, write or dump memory outside its size range

--- gameboy/memory.go
+++ gameboy/memory.go
@@ -1,32 +1,85 @@
 package gameboy
 
+import "math/rand"
+
+/**
+ * Memory struct represents any memory type RAM or ROM in the GameBoy.
+ * It is used to hold uint8 values and is indexed by a uint16 address.
+ * It is used here to represent the BOOTROM, cartridge ROM and RAM, Video RAM, Working RAM, High RAM, OAM, ...
+ * While CPU registers are defined inside the CPU struct as []uint8/[]uint16, the IE register is defined as a memory cell.
+ */
 type Memory struct {
 	data []uint8
 }
 
+/**
+ * NewMemory creates a new Memory object with the given size.
+ * All memory locations are initialized to 0.
+ */
 func NewMemory(size uint16) *Memory {
 	return &Memory{data: make([]uint8, size)}
 }
 
-func (m *Memory) setData(data []uint8) {
-	m.data = data
+/**
+ * NewMemoryWithData creates a new Memory object with the given size and data.
+ * The data is copied into the memory. The data size must match the memory size.
+ */
+func NewMemoryWithData(size uint16, data []uint8) *Memory {
+	if len(data) != int(size) {
+		panic("Data size does not match memory size")
+	}
+	return &Memory{data: data}
 }
 
+/**
+ * NewMemoryWithRandomData creates a new Memory object with the given size and random data.
+ * It is used to simulate the initial state of the memories in the gameboy which state cannot be predicted.
+ * It allows us to see the bootrom clearing the VRAM on startup.
+ */
+func NewMemoryWithRandomData(size uint16) *Memory {
+	data := make([]uint8, size)
+	for i := 0; i < len(data); i++ {
+		data[i] = uint8(rand.Intn(256))
+	}
+	return &Memory{data: data}
+}
+
+/**
+ * returns the size of the memory in bytes.
+ */
 func (m *Memory) Size() uint16 {
 	return uint16(len(m.data))
 }
 
+/**
+ * Read returns the uint8 value at the given address.
+ * panics if the address is out of bounds.
+ */
 func (m *Memory) Read(addr uint16) uint8 {
+	if addr >= uint16(len(m.data)) {
+		panic("Memory address out of bounds while reading")
+	}
 	return m.data[addr]
 }
 
-/*
+/**
  * Dump memory from address 'from' to address 'to'
+ * panics if the addresses are out of bounds.
  */
 func (m *Memory) Dump(from uint16, to uint16) []uint8 {
+	if from >= m.Size() || to >= m.Size() {
+		panic("Memory address out of bounds while dumping")
+	}
 	return m.data[from:to]
 }
 
+/**
+ * Write the provided uint8 value at the given address.
+ * panics if the address is out of bounds.
+ */
 func (m *Memory) Write(addr uint16, value uint8) {
+	if addr >= uint16(len(m.data)) {
+		panic("Memory address out of bounds while writing")
+	}
 	m.data[addr] = value
 }
--- test/memory_test.go
+++ test/memory_test.go
@@ -9,23 +9,91 @@ import (
 func TestNewMemory(t *testing.T) {
 	memory := gameboy.NewMemory(0x2000)
 	if memory == nil {
-		t.Error("Expected VRAM to be initialized")
+		t.Error("Expected memory to be initialized")
 	}
 	if memory.Size() != 0x2000 {
-		t.Errorf("Expected VRAM to have 0x2000 bytes, got %d", memory.Size())
+		t.Errorf("Expected memory to have 0x2000 bytes, got %d", memory.Size())
+	}
+	for i := 0; i < 0x2000; i++ {
+		if memory.Read(uint16(i)) != 0 {
+			t.Errorf("Expected memory to be initialized with 0 at address %04X, got %02X", i, memory.Read(uint16(i)))
+		}
 	}
 }
 
-// func TestVRAMReadAndWrite(t *testing.T) {
-func TestReadAndWrite(t *testing.T) {
-	memory := gameboy.NewMemory(0x2000)
-	for i := 0; i < 0x2000; i++ {
-		memory.Write(uint16(i), uint8(i))
+func TestNewMemoryWithData(t *testing.T) {
+	data := make([]uint8, 256)
+	for i := 0; i < 256; i++ {
+		data[i] = uint8(i)
 	}
-	for i := 0; i < 0x2000; i++ {
+	memory := gameboy.NewMemoryWithData(0x0100, data)
+	for i := 0; i < 256; i++ {
+		memoryCellData := memory.Read(uint16(i))
+		if memoryCellData != uint8(i) {
+			t.Errorf("Expected memory to be initialized with %02X at address %04X, got %02X", i, i, memoryCellData)
+		}
+	}
+}
+
+func TestNewMemoryWithDataUnmatchingSize(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Expected NewMemoryWithData to panic when data size does not match memory size")
+		}
+	}()
+	data := make([]uint8, 256)
+	gameboy.NewMemoryWithData(0xFFFF, data)
+}
+
+func TestNewMemoryWithRandomData(t *testing.T) {
+	memorySize := uint16(0x2000)
+	memory := gameboy.NewMemoryWithRandomData(memorySize)
+	// check that all data are not 0 and that they are random
+	freq := make(map[uint8]int)
+	for i := 0; i < int(memorySize); i++ {
 		data := memory.Read(uint16(i))
-		if data != uint8(i) {
-			t.Errorf("Expected VRAM to have value %02X at address %04X, got %02X", i, i, data)
+		freq[data]++
+	}
+	for k, v := range freq {
+		if v == 0 {
+			t.Errorf("Expected memory to have random data, but %02X is missing", k)
+		}
+	}
+}
+
+func TestMemoryReadOutsideRange(t *testing.T) {
+	// it shouldn't be possible to read outside the memory range
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Expected Read to panic when reading outside the memory range")
+		}
+	}()
+	memory := gameboy.NewMemory(10)
+	memory.Read(11)
+}
+
+func TestMemoryWriteOutsideRange(t *testing.T) {
+	// it shouldn't be possible to write outside the memory range
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("Expected Write to panic when writing outside the memory range")
+		}
+	}()
+	memory := gameboy.NewMemory(10)
+	memory.Write(11, 0)
+}
+
+func TestMemoryDump(t *testing.T) {
+	data := make([]uint8, 256)
+	for i := 0; i < 256; i++ {
+		data[i] = uint8(i)
+	}
+	memory := gameboy.NewMemoryWithData(0x100, data)
+	dump := memory.Dump(0, 256)
+	for i := 0; i < 256; i++ {
+		memoryCellData := dump[i]
+		if memoryCellData != uint8(i) {
+			t.Errorf("Expected memory to be initialized with %02X at address %04X, got %02X", i, i, memoryCellData)
 		}
 	}
 }
