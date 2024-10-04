d87643063fdbaa0b41548ee4cfa5b220ce903b34
codeFrite <codefrite@gmail.com>
2024-10-04 19:20:18 +0200

    moving memory write to its own go file // adding tests for memory write custom marshal func

--- gameboy/mmu.go
+++ gameboy/mmu.go
@@ -1,10 +1,8 @@
 package gameboy
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"strings"
 )
 
 type Accessible interface {
@@ -24,34 +22,6 @@ type MemoryMap struct {
 	Name    string
 	Address uint16
 	Memory  Accessible
-}
-
-/**
- * JSONableSlice is a type alias for a slice of uint8 used to make it JSONable by defining a custom JSON marshalling method.
- */
-type JSONableSlice []uint8
-
-/**
- * represents a memory write operation used by the mmu to keep track of all memory changes between 2 states
- * memory writes can be either reset:
- * - before every step from the debugger
- * - before a new run from a breakpoint to another breakpoint
- */
-type MemoryWrite struct {
-	Name    string        `json:"name"`
-	Address uint16        `json:"address"`
-	Data    JSONableSlice `json:"data"`
-}
-
-// MarshalJSON is a custom JSON marshalling method for the JSONableSlice type
-// It converts the slice of uint8 to a string of comma-separated values
-func (j JSONableSlice) MarshalJSON() ([]byte, error) {
-	strValues := make([]string, len(j))
-	for i, v := range j {
-		strValues[i] = fmt.Sprintf("%d", v)
-	}
-	result := strings.Join(strValues, ",")
-	return json.Marshal(result)
 }
 
 /**
