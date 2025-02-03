d5365b0b04334aeb1de956b504bf76fe9dacd2db
codeFrite <codefrite@gmail.com>
2025-02-03 09:35:15 +0100

    the ppu.getState() interface changed: it now returns the state along with an error

--- gameboy/ppu_state.go
+++ gameboy/ppu_state.go
@@ -1,17 +1,24 @@
 package gameboy
 
+import "errors"
+
 type PpuState struct {
-	MODE  uint8 `json:"MODE"`
-	DOT_X uint8 `json:"DOT_X"`
-	DOT_Y uint8 `json:"DOT_Y"`
-	IMAGE Image `json:"IMAGE"`
+	MODE  uint8         `json:"MODE"`
+	DOT_X uint8         `json:"DOT_X"`
+	DOT_Y uint8         `json:"DOT_Y"`
+	IMAGE RenderedImage `json:"IMAGE"`
 }
 
-func (p *PPU) getState() PpuState {
-	return PpuState{
-		MODE:  p.mode,
-		DOT_X: p.dotX,
-		DOT_Y: p.dotY,
-		IMAGE: p.image,
+// Returns the current state of the PPU if the PPU is enabled
+func (p *PPU) getState() (*PpuState, error) {
+	if p.isEnabled() {
+		return &PpuState{
+			MODE:  p.mode,
+			DOT_X: p.dotX,
+			DOT_Y: p.dotY,
+			IMAGE: p.image,
+		}, nil
+	} else {
+		return nil, errors.New("PPU is disabled")
 	}
 }
